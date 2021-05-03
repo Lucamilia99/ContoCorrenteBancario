d13ab97433baf2602993e5aca5dc797ea0ed4852
Ruggero8 <Raponi.ruggero7@gmail.com>
2021-05-03 17:00:37 +0200

    Update app.component.ts

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ContoCorrente } from './conto-corrente';
 import { ContoCorrenteDto } from './conto-corrente-dto';
@@ -8,9 +9,10 @@ import { ContoCorrenteDto } from './conto-corrente-dto';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
+  url="http://localhost:8080/";
 
   contoCorrente = new ContoCorrente();
 
@@ -19,6 +21,9 @@ export class AppComponent {
   aggiungi() {
     let dto = new ContoCorrenteDto();
     dto.contoCorrente = this.contoCorrente;
+    this.http.post<ContoCorrenteDto>(this.url + "aggiungi", dto)
+    .subscribe(f=>this.contoCorrente=f.contoCorrente)
+    
 
   }
 }
